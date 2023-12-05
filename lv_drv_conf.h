c3d4c579c0a3880550ac5a6c504cdd1e6f566147
Daniel Glinka <dgl@degit.co>
2023-12-05 16:01:21 -0300

    Use sdl

--- lv_drv_conf.h
+++ lv_drv_conf.h
@@ -86,12 +86,12 @@
 
 /* SDL based drivers for display, mouse, mousewheel and keyboard*/
 #ifndef USE_SDL
-# define USE_SDL 0
+# define USE_SDL 1
 #endif
 
 /* Hardware accelerated SDL driver */
 #ifndef USE_SDL_GPU
-# define USE_SDL_GPU 0
+# define USE_SDL_GPU 1
 #endif
 
 #if USE_SDL || USE_SDL_GPU
@@ -316,7 +316,7 @@
  *  Linux frame buffer device (/dev/fbx)
  *-----------------------------------------*/
 #ifndef USE_FBDEV
-#  define USE_FBDEV           1
+#  define USE_FBDEV           0
 #endif
 
 #if USE_FBDEV
--- main.c
+++ main.c
@@ -1,82 +1,78 @@
-#include "lvgl/lvgl.h"
-#include "lvgl/demos/lv_demos.h"
-#include "lv_drivers/display/fbdev.h"
-#include "lv_drivers/indev/evdev.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <time.h>
 #include <sys/time.h>
+#include "lvgl/lvgl.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <signal.h>
+#define SLEEP(s) usleep(s * 1000)
 
-#define DISP_BUF_SIZE (128 * 1024)
+static int demo_status;
 
-int main(void)
-{
-    /*LittlevGL init*/
-    lv_init();
+static void demo_release(int signal) {
+    printf("get singal[%d]\n", signal);
+    demo_status = 0;
+}
 
-    /*Linux frame buffer device init*/
-    fbdev_init();
+static void create_ui(void) {
+    lv_obj_t *btn = lv_btn_create(lv_scr_act());
+    lv_obj_t *lbl = lv_label_create(btn);
+    lv_obj_set_style_text_font(lbl, &lv_font_simsun_16_cjk, 0);
 
-    /*A small buffer for LittlevGL to draw the screen's content*/
-    static lv_color_t buf[DISP_BUF_SIZE];
+    lv_label_set_text(lbl, "Hello world!");
 
-    /*Initialize a descriptor for the buffer*/
+    lv_obj_center(lbl);
+    lv_obj_center(btn);
+}
+
+static void driver_init(void) {
+#define BUFFER_SIZE (SDL_HOR_RES * SDL_VER_RES)
+    /*A static or global variable to store the buffers*/
     static lv_disp_draw_buf_t disp_buf;
-    lv_disp_draw_buf_init(&disp_buf, buf, NULL, DISP_BUF_SIZE);
 
-    /*Initialize and register a display driver*/
-    static lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    disp_drv.draw_buf   = &disp_buf;
-    disp_drv.flush_cb   = fbdev_flush;
-    disp_drv.hor_res    = 800;
-    disp_drv.ver_res    = 480;
-    lv_disp_drv_register(&disp_drv);
+    /*Static or global buffer(s). The second buffer is optional*/
+    static lv_color_t *buf_1[BUFFER_SIZE] = {0};
 
-    evdev_init();
-    static lv_indev_drv_t indev_drv_1;
-    lv_indev_drv_init(&indev_drv_1); /*Basic initialization*/
-    indev_drv_1.type = LV_INDEV_TYPE_POINTER;
+    /*Initialize `disp_buf` with the buffer(s). With only one buffer use NULL instead buf_2 */
+    lv_disp_draw_buf_init(&disp_buf, buf_1, NULL, BUFFER_SIZE);
 
-    /*This function will be called periodically (by the library) to get the mouse position and state*/
-    indev_drv_1.read_cb = evdev_read;
-    lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
+    static lv_disp_drv_t disp_drv;         /*A variable to hold the drivers. Must be static or global.*/
+    lv_disp_drv_init(&disp_drv);           /*Basic initialization*/
+    disp_drv.draw_buf = &disp_buf;         /*Set an initialized buffer*/
+    disp_drv.flush_cb = sdl_display_flush; /*Set a flush callback to draw to the display*/
+    disp_drv.hor_res  = SDL_HOR_RES;       /*Set the horizontal resolution in pixels*/
+    disp_drv.ver_res  = SDL_VER_RES;       /*Set the vertical resolution in pixels*/
 
+    lv_disp_t *disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
+    lv_theme_default_init(disp, lv_color_make(0x77, 0x44, 0xBB), lv_color_make(0x14, 0x14, 0x3C), 1, lv_font_default());
 
-    /*Set a cursor for the mouse*/
-    LV_IMG_DECLARE(mouse_cursor_icon)
-    lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
-    lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-    lv_indev_set_cursor(mouse_indev, cursor_obj);             /*Connect the image  object to the driver*/
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv); /*Basic initialization*/
+    indev_drv.type    = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = sdl_mouse_read;
+    lv_indev_drv_register(&indev_drv);
+}
 
+int main(void) {
 
-    /*Create a Demo*/
-    lv_demo_widgets();
+    demo_status = 1;
+    signal(SIGINT, demo_release);
+    signal(SIGTERM, demo_release);
 
-    /*Handle LitlevGL tasks (tickless mode)*/
-    while(1) {
+    lv_init();
+    sdl_init();
+
+    driver_init();
+    create_ui();
+
+    while(demo_status) {
+        // Run LVGL engine
+        lv_tick_inc(1);
         lv_timer_handler();
-        usleep(5000);
+
+        SLEEP(1);
     }
 
     return 0;
-}
-
-/*Set in lv_conf.h as `LV_TICK_CUSTOM_SYS_TIME_EXPR`*/
-uint32_t custom_tick_get(void)
-{
-    static uint64_t start_ms = 0;
-    if(start_ms == 0) {
-        struct timeval tv_start;
-        gettimeofday(&tv_start, NULL);
-        start_ms = (tv_start.tv_sec * 1000000 + tv_start.tv_usec) / 1000;
-    }
-
-    struct timeval tv_now;
-    gettimeofday(&tv_now, NULL);
-    uint64_t now_ms;
-    now_ms = (tv_now.tv_sec * 1000000 + tv_now.tv_usec) / 1000;
-
-    uint32_t time_ms = now_ms - start_ms;
-    return time_ms;
 }
